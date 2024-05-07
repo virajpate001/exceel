b59c5d2dbae7d0c30eff6d4d44bf99f5a16bea76
Viraj Pate <149562537+virajpate001@users.noreply.github.com>
2024-05-07 11:23:10 +0530

    Add files via upload

---
+++ revolution/rslider.js
@@ -0,0 +1,317 @@
+/* Slider 1*/
+var	tpj = jQuery;
+	if(window.RS_MODULES === undefined) window.RS_MODULES = {};
+	if(RS_MODULES.modules === undefined) RS_MODULES.modules = {};
+	RS_MODULES.modules["revslider11"] = {once: RS_MODULES.modules["revslider11"]!==undefined ? RS_MODULES.modules["revslider11"].once : undefined, init:function() {
+		window.revapi1 = window.revapi1===undefined || window.revapi1===null || window.revapi1.length===0  ? document.getElementById("rev_slider_1_1") : window.revapi1;
+		if(window.revapi1 === null || window.revapi1 === undefined || window.revapi1.length==0) { window.revapi1initTry = window.revapi1initTry ===undefined ? 0 : window.revapi1initTry+1; if (window.revapi1initTry<20) requestAnimationFrame(function() {RS_MODULES.modules["revslider11"].init()}); return;}
+		window.revapi1 = jQuery(window.revapi1);
+		if(window.revapi1.revolution==undefined){ revslider_showDoubleJqueryError("rev_slider_1_1"); return;}
+		revapi1.revolutionInit({
+				revapi:"revapi1",
+				DPR:"dpr",
+				sliderLayout:"fullwidth",
+				visibilityLevels:"1240,1024,778,480",
+				gridwidth:"1230,1024,778,480",
+				gridheight:"750,650,550,400",
+				perspective:600,
+				perspectiveType:"global",
+				editorheight:"750,650,550,400",
+				responsiveLevels:"1240,1024,778,480",
+				progressBar:{disableProgressBar:true},
+				navigation: {
+					wheelCallDelay:1000,
+					onHoverStop:false,
+					arrows: {
+						enable:true,
+						style:"metis",
+						hide_onmobile:true,
+						hide_under:"767px",
+						left: {
+							h_align:"right",
+							v_align:"bottom",
+							h_offset:60
+						},
+						right: {
+							v_align:"bottom",
+							h_offset:0
+						}
+					}
+				},
+				viewPort: {
+					global:false,
+					globalDist:"-200px",
+					enable:false
+				},
+				fallbacks: {
+					allowHTML5AutoPlayOnAndroid:true
+				},
+		});
+		
+	}} // End of RevInitScript
+	if (window.RS_MODULES.checkMinimal!==undefined) { window.RS_MODULES.checkMinimal();};
+
+/* Slider 2*/ 
+var	tpj = jQuery;
+	if(window.RS_MODULES === undefined) window.RS_MODULES = {};
+	if(RS_MODULES.modules === undefined) RS_MODULES.modules = {};
+	RS_MODULES.modules["revslider21"] = {once: RS_MODULES.modules["revslider21"]!==undefined ? RS_MODULES.modules["revslider21"].once : undefined, init:function() {
+		window.revapi2 = window.revapi2===undefined || window.revapi2===null || window.revapi2.length===0  ? document.getElementById("rev_slider_2_1") : window.revapi2;
+		if(window.revapi2 === null || window.revapi2 === undefined || window.revapi2.length==0) { window.revapi2initTry = window.revapi2initTry ===undefined ? 0 : window.revapi2initTry+1; if (window.revapi2initTry<20) requestAnimationFrame(function() {RS_MODULES.modules["revslider21"].init()}); return;}
+		window.revapi2 = jQuery(window.revapi2);
+		if(window.revapi2.revolution==undefined){ revslider_showDoubleJqueryError("rev_slider_2_1"); return;}
+		revapi2.revolutionInit({
+				revapi:"revapi2",
+				DPR:"dpr",
+				sliderLayout:"fullwidth",
+				visibilityLevels:"1240,1024,778,480",
+				gridwidth:"1230,1024,778,480",
+				gridheight:"790,670,580,400",
+				perspective:600,
+				perspectiveType:"global",
+				editorheight:"790,670,580,400",
+				responsiveLevels:"1240,1024,778,480",
+				progressBar:{disableProgressBar:true},
+				navigation: {
+					wheelCallDelay:1000,
+					onHoverStop:false,
+					arrows: {
+						enable:true,
+						style:"metis",
+						hide_onmobile:true,
+						hide_under:"767px",
+						left: {
+							h_align:"right",
+							v_align:"bottom",
+							h_offset:60
+						},
+						right: {
+							v_align:"bottom",
+							h_offset:0
+						}
+					}
+				},
+				viewPort: {
+					global:false,
+					globalDist:"-200px",
+					enable:false
+				},
+				fallbacks: {
+					allowHTML5AutoPlayOnAndroid:true
+				},
+		});
+		
+	}} // End of RevInitScript
+	if (window.RS_MODULES.checkMinimal!==undefined) { window.RS_MODULES.checkMinimal();};
+
+/* Slider 3*/ 
+var	tpj = jQuery;
+	if(window.RS_MODULES === undefined) window.RS_MODULES = {};
+	if(RS_MODULES.modules === undefined) RS_MODULES.modules = {};
+	RS_MODULES.modules["revslider33"] = {once: RS_MODULES.modules["revslider33"]!==undefined ? RS_MODULES.modules["revslider33"].once : undefined, init:function() {
+		window.revapi3 = window.revapi3===undefined || window.revapi3===null || window.revapi3.length===0  ? document.getElementById("rev_slider_3_1") : window.revapi3;
+		if(window.revapi3 === null || window.revapi3 === undefined || window.revapi3.length==0) { window.revapi3initTry = window.revapi3initTry ===undefined ? 0 : window.revapi3initTry+1; if (window.revapi3initTry<20) requestAnimationFrame(function() {RS_MODULES.modules["revslider33"].init()}); return;}
+		window.revapi3 = jQuery(window.revapi3);
+		if(window.revapi3.revolution==undefined){ revslider_showDoubleJqueryError("rev_slider_3_1"); return;}
+		revapi3.revolutionInit({
+				revapi:"revapi3",
+				DPR:"dpr",
+				sliderLayout:"fullwidth",
+				visibilityLevels:"1240,1024,778,480",
+				gridwidth:"1230,1024,778,480",
+				gridheight:"780,650,550,400",
+				perspective:600,
+				perspectiveType:"global",
+				editorheight:"780,650,550,400",
+				responsiveLevels:"1240,1024,778,480",
+				progressBar:{disableProgressBar:true},
+				navigation: {
+					wheelCallDelay:1000,
+					onHoverStop:false,
+					arrows: {
+						enable:true,
+						style:"metis",
+						hide_onmobile:true,
+						hide_under:"1024px",
+						left: {
+							h_align:"right",
+							v_align:"bottom",
+							h_offset:60
+						},
+						right: {
+							v_align:"bottom",
+							h_offset:0
+						}
+					}
+				},
+				viewPort: {
+					global:false,
+					globalDist:"-200px",
+					enable:false
+				},
+				fallbacks: {
+					allowHTML5AutoPlayOnAndroid:true
+				},
+		});
+		
+	}} // End of RevInitScript
+	if (window.RS_MODULES.checkMinimal!==undefined) { window.RS_MODULES.checkMinimal();};
+
+/* Slider 4*/ 
+var	tpj = jQuery;
+	if(window.RS_MODULES === undefined) window.RS_MODULES = {};
+	if(RS_MODULES.modules === undefined) RS_MODULES.modules = {};
+	RS_MODULES.modules["revslider44"] = {once: RS_MODULES.modules["revslider44"]!==undefined ? RS_MODULES.modules["revslider44"].once : undefined, init:function() {
+		window.revapi4 = window.revapi4===undefined || window.revapi4===null || window.revapi4.length===0  ? document.getElementById("rev_slider_4_1") : window.revapi4;
+		if(window.revapi4 === null || window.revapi4 === undefined || window.revapi4.length==0) { window.revapi4initTry = window.revapi4initTry ===undefined ? 0 : window.revapi4initTry+1; if (window.revapi4initTry<20) requestAnimationFrame(function() {RS_MODULES.modules["revslider44"].init()}); return;}
+		window.revapi4 = jQuery(window.revapi4);
+		if(window.revapi4.revolution==undefined){ revslider_showDoubleJqueryError("rev_slider_4_1"); return;}
+		revapi4.revolutionInit({
+				revapi:"revapi4",
+				DPR:"dpr",
+				sliderLayout:"fullwidth",
+				visibilityLevels:"1240,1024,778,480",
+				gridwidth:"1230,1024,778,480",
+				gridheight:"850,650,550,400",
+				perspective:600,
+				perspectiveType:"global",
+				editorheight:"850,650,550,400",
+				responsiveLevels:"1240,1024,778,480",
+				progressBar:{disableProgressBar:true},
+				navigation: {
+					wheelCallDelay:1000,
+					onHoverStop:false,
+					arrows: {
+						enable:true,
+						style:"metis",
+						hide_onmobile:true,
+						hide_under:"767px",
+						left: {
+							h_align:"right",
+							v_align:"bottom",
+							h_offset:60
+						},
+						right: {
+							v_align:"bottom",
+							h_offset:0
+						}
+					}
+				},
+				viewPort: {
+					global:false,
+					globalDist:"-200px",
+					enable:false
+				},
+				fallbacks: {
+					allowHTML5AutoPlayOnAndroid:true
+				},
+		});
+		
+	}} // End of RevInitScript
+	if (window.RS_MODULES.checkMinimal!==undefined) { window.RS_MODULES.checkMinimal();};
+
+/* Slider 5*/
+var	tpj = jQuery;
+	if(window.RS_MODULES === undefined) window.RS_MODULES = {};
+	if(RS_MODULES.modules === undefined) RS_MODULES.modules = {};
+	RS_MODULES.modules["revslider55"] = {once: RS_MODULES.modules["revslider55"]!==undefined ? RS_MODULES.modules["revslider55"].once : undefined, init:function() {
+		window.revapi5 = window.revapi5===undefined || window.revapi5===null || window.revapi5.length===0  ? document.getElementById("rev_slider_5_1") : window.revapi5;
+		if(window.revapi5 === null || window.revapi5 === undefined || window.revapi5.length==0) { window.revapi5initTry = window.revapi5initTry ===undefined ? 0 : window.revapi5initTry+1; if (window.revapi5initTry<20) requestAnimationFrame(function() {RS_MODULES.modules["revslider55"].init()}); return;}
+		window.revapi5 = jQuery(window.revapi5);
+		if(window.revapi5.revolution==undefined){ revslider_showDoubleJqueryError("rev_slider_5_1"); return;}
+		revapi5.revolutionInit({
+				revapi:"revapi5",
+				DPR:"dpr",
+				sliderLayout:"fullwidth",
+				visibilityLevels:"1240,1024,778,480",
+				gridwidth:"1230,1024,778,480",
+				gridheight:"760,650,550,400",
+				perspective:600,
+				perspectiveType:"global",
+				editorheight:"760,650,550,400",
+				responsiveLevels:"1240,1024,778,480",
+				progressBar:{disableProgressBar:true},
+				navigation: {
+					wheelCallDelay:1000,
+					onHoverStop:false,
+					arrows: {
+						enable:true,
+						style:"metis",
+						hide_onmobile:true,
+						hide_under:"767px",
+						left: {
+							h_align:"right",
+							v_align:"bottom",
+							h_offset:60
+						},
+						right: {
+							v_align:"bottom",
+							h_offset:0
+						}
+					}
+				},
+				viewPort: {
+					global:false,
+					globalDist:"-200px",
+					enable:false
+				},
+				fallbacks: {
+					allowHTML5AutoPlayOnAndroid:true
+				},
+		});
+		
+	}} // End of RevInitScript
+	if (window.RS_MODULES.checkMinimal!==undefined) { window.RS_MODULES.checkMinimal();};
+
+	/* Slider 6*/
+	var	tpj = jQuery;
+		if(window.RS_MODULES === undefined) window.RS_MODULES = {};
+		if(RS_MODULES.modules === undefined) RS_MODULES.modules = {};
+		RS_MODULES.modules["revslider66"] = {once: RS_MODULES.modules["revslider66"]!==undefined ? RS_MODULES.modules["revslider66"].once : undefined, init:function() {
+			window.revapi6 = window.revapi6===undefined || window.revapi6===null || window.revapi6.length===0  ? document.getElementById("rev_slider_6_1") : window.revapi6;
+			if(window.revapi6 === null || window.revapi6 === undefined || window.revapi6.length==0) { window.revapi6initTry = window.revapi6initTry ===undefined ? 0 : window.revapi6initTry+1; if (window.revapi6initTry<20) requestAnimationFrame(function() {RS_MODULES.modules["revslider66"].init()}); return;}
+			window.revapi6 = jQuery(window.revapi6);
+			if(window.revapi6.revolution==undefined){ revslider_showDoubleJqueryError("rev_slider_6_1"); return;}
+			revapi6.revolutionInit({
+					revapi:"revapi6",
+					DPR:"dpr",
+					sliderLayout:"fullwidth",
+					visibilityLevels:"1240,1024,778,480",
+					gridwidth:"1230,1024,778,480",
+					gridheight:"760,650,550,400",
+					perspective:600,
+					perspectiveType:"global",
+					editorheight:"760,650,550,400",
+					responsiveLevels:"1240,1024,778,480",
+					progressBar:{disableProgressBar:true},
+					navigation: {
+						wheelCallDelay:1000,
+						onHoverStop:false,
+						arrows: {
+							enable:true,
+							style:"metis",
+							hide_onmobile:true,
+							hide_under:"767px",
+							left: {
+								h_align:"right",
+								v_align:"bottom",
+								h_offset:60
+							},
+							right: {
+								v_align:"bottom",
+								h_offset:0
+							}
+						}
+					},
+					viewPort: {
+						global:false,
+						globalDist:"-200px",
+						enable:false
+					},
+					fallbacks: {
+						allowHTML5AutoPlayOnAndroid:true
+					},
+			});
+			
+		}} // End of RevInitScript
+		if (window.RS_MODULES.checkMinimal!==undefined) { window.RS_MODULES.checkMinimal();};
